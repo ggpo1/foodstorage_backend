64e673a4497ceb7c62598ee6a4c5f1b89de88846
v01 <johnlevis199@gmail.com>
2019-08-06 17:52:13 +0300

    Product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using FoodStorage_Backend.Models.DataBase;
+using FoodStorage_Backend.Services.ProductService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodStorage_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController
+    {
+        IProductService service;
+
+        public ProductController(IProductService service)
+        {
+            this.service = service;
+        }
+
+        #region Get data methods
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            return await service.GetProducts();
+        }
+
+        [HttpGet]
+        [Route("getproductbyid")]
+        public async Task<Product> GetProdutByID(int productID)
+        {
+            return await service.GetProduct(productID);
+        }
+
+        [HttpGet]
+        [Route("getproducttypeproducts")]
+        public async Task<List<Product>> GetProdutTypeProducts(int productTypeID)
+        {
+            return await service.GetProductTypeProducts(productTypeID);
+        }
+
+        #endregion
+        #region Post data methods
+        [HttpPost]
+        public async Task<Product> AddProduct([FromBody]Product product)
+        {
+            return await service.AddProduct(product);
+        }
+        // TODO: UPDATE METHOD
+        #endregion
+    }
+}
